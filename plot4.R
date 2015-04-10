2bcf1f57a3159fd0d4950d74ebf38fac4f185064
Ken Lin <github.kenklin@kenlin.com>
2015-04-09 21:31:11 -0400

    Completed plot4.R

--- plot4.R
+++ plot4.R
@@ -1,17 +1,20 @@
 # https://class.coursera.org/exdata-013/human_grading/view/courses/973507/assessments/3/submissions
 
-plot1.read <- function(fn) {
+plot4.read <- function(fn) {
   df <- read.csv(fn, sep=";", stringsAsFactors=FALSE)
   return(df)
 }
 
-plot1.tidy <- function(df) {
+plot4.tidy <- function(df) {
   require(dplyr)
   tbl <- tbl_df(df)
   
   # We will only be using data from the dates 2007-02-01 and 2007-02-02.
-  tbl$Date <- as.Date(tbl$Date, "%d/%m/%Y")
-  tbl <- filter(tbl, Date >= "2007-02-01" & Date <= "2007-02-02")
+  tbl$DateObj <- as.Date(tbl$Date, "%d/%m/%Y")
+  tbl <- filter(tbl, DateObj >= "2007-02-01" & DateObj <= "2007-02-02")
+  
+  # Create new datetime column
+  tbl$DateTime <- strptime(paste(tbl$Date, tbl$Time), "%d/%m/%Y %H:%M:%S")
   
   # Coerce remaining variables
   tbl$Global_active_power   <- as.numeric(tbl$Global_active_power)
@@ -25,29 +28,40 @@ plot1.tidy <- function(df) {
   return(tbl)
 }
 
-plot1.plot <- function(tbl) {
-  with(tbl, hist(Global_active_power, 
-                 main="Global Active Power",
-                 xlab="Global Active Power (kilowatts)",
-                 ylab="Frequency",
-                 col="red"))
+plot4Voltage.plot <- function(tbl) {
+  with(tbl, plot(DateTime, Voltage,
+                 type="l",
+                 ylab="Voltage",
+                 xlab="datetime"))
 }
 
-plot1.main <- function() {
+plot4GlobalReactivePower.plot <- function(tbl) {
+  with(tbl, plot(DateTime, Global_reactive_power,
+                 type="l",
+                 xlab="datetime"))
+}
+
+plot4.main <- function() {
   # Set environment
   setwd("/Users/klin/Documents/coursera/exdata-013/ExData_Plotting1")
   input <- "data/household_power_consumption.txt"
-  output <- "plot1.png"
+  output <- "plot4.png"
   
   # Read, filter, and tidy into tbl
-  df <- plot1.read(input)
-  tbl <- plot1.tidy(df)
-
+#  df <- plot4.read(input)
+  tbl <- plot4.tidy(df)
+  
   # To avoid screen to png copy anomolies, write directly to png per ...
   # https://class.coursera.org/exdata-013/forum/thread?thread_id=14
   png(output, width=480, height=480)
-  plot1.plot(tbl)
+  par(mfrow = c(2, 2))
+  with(tbl, {
+    plot2.plot(tbl)             # plot2.R must have already been sourced!!!
+    plot4Voltage.plot(tbl)
+    plot3.plot(tbl)             # plot3.R must have already been sourced!!!
+    plot4GlobalReactivePower.plot(tbl)
+  })
   dev.off()
 }
 
-plot1.main()
+plot4.main()
